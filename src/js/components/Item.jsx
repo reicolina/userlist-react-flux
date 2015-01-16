ae18344271365736e88b112e77bcfc4b89d32ff5
Reinaldo Colina <rcolina@pingidentity.com>
2015-01-15 16:58:27 -0800

    cosmetic fixes

--- src/js/components/Item.jsx
+++ src/js/components/Item.jsx
@@ -9,7 +9,7 @@ var Item = React.createClass({
     render: function () {
         var item = this.props.item;
         return (
-            <tr key={item.id}><td>{item.name}</td></tr>
+            <tr key={item.id}><td>{item.name}</td><td><button className={"btn btn-danger btn-xs pull-right"}>x</button></td></tr>
         );
     },
 
